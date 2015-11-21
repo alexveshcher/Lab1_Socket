f713159d241b056c20a9881cc09265ed043a4601
alexveshcher <s.veshcherovych@gmail.com>
2015-11-21 16:27:57 +0200

    removed all rmi clear project without rmi

--- src/dao/BookDao.java
+++ src/dao/BookDao.java
@@ -8,11 +8,11 @@ import java.util.List;
 import vo.Book;
 
 
-public interface BookDao extends Remote {
-	public Book create() throws RemoteException;
-	public Book read(int key) throws SQLException,RemoteException;
-	public void update(Book book)throws RemoteException;
-	public void delete(Book book)throws RemoteException;
-	public List<Book> getAllBooks() throws SQLException,RemoteException;
-	public List<Book> search(String searchWord) throws SQLException,RemoteException;
+public interface BookDao{
+	public Book create() ;
+	public Book read(int key) throws SQLException;
+	public void update(Book book);
+	public void delete(Book book);
+	public List<Book> getAllBooks() throws SQLException;
+	public List<Book> search(String searchWord) throws SQLException;
 }
