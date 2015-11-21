009b51cc5b0bb3fd0e09453dfcf3d77042124a00
alexveshcher <s.veshcherovych@gmail.com>
2015-11-21 13:55:27 +0200

    returning to work removed all socket dependencies

--- src/dao/BookDao.java
+++ src/dao/BookDao.java
@@ -1,15 +1,18 @@
 package dao;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.sql.SQLException;
 import java.util.List;
 
 import vo.Book;
 
-public interface BookDao {
-	public Book create();
-	public Book read(int key) throws SQLException;
-	public void update(Book book);
-	public void delete(Book book);
-	public List<Book> getAllBooks() throws SQLException;
-	public List<Book> search(String searchWord) throws SQLException;
+
+public interface BookDao extends Remote {
+	public Book create() throws RemoteException;
+	public Book read(int key) throws SQLException,RemoteException;
+	public void update(Book book)throws RemoteException;
+	public void delete(Book book)throws RemoteException;
+	public List<Book> getAllBooks() throws SQLException,RemoteException;
+	public List<Book> search(String searchWord) throws SQLException,RemoteException;
 }
